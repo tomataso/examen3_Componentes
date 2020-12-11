f465640e41aded2e5b633b90ec29080710924adc
tomataso <tomataso@users.noreply.github.com>
2020-12-11 00:17:48 -0600

    Clase Modelo / Dependencias / Properties / BD

---
+++ RestApi/ExamenApi/src/main/java/com/cenfotec/examentres/ExamenApiApplication.java
@@ -0,0 +1,13 @@
+package com.cenfotec.examentres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenApiApplication.class, args);
+	}
+
+}
---
+++ RestApi/ExamenApi/src/main/java/com/cenfotec/examentres/model/Cliente.java
@@ -0,0 +1,78 @@
+package com.cenfotec.examentres.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class Cliente {
+	
+	@Id
+	 @GeneratedValue(strategy = GenerationType.IDENTITY)
+	 private Long id;
+	
+	 private String nombre;
+	 private String apellidos;
+	 private String domicilio;
+	 private String cobro;
+	 private String tarjeta;
+	 private int mes;
+	 private int anno;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellidos() {
+		return apellidos;
+	}
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+	public String getDomicilio() {
+		return domicilio;
+	}
+	public void setDomicilio(String domicilio) {
+		this.domicilio = domicilio;
+	}
+	public String getCobro() {
+		return cobro;
+	}
+	public void setCobro(String cobro) {
+		this.cobro = cobro;
+	}
+	public String getTarjeta() {
+		return tarjeta;
+	}
+	public void setTarjeta(String tarjeta) {
+		this.tarjeta = tarjeta;
+	}
+	public int getMes() {
+		return mes;
+	}
+	public void setMes(int mes) {
+		this.mes = mes;
+	}
+	public int getAnno() {
+		return anno;
+	}
+	public void setAnno(int anno) {
+		this.anno = anno;
+	}
+
+
+}
